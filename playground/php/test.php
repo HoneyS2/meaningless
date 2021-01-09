2fe166976c98c38682fa656bd22a7c05dcb7ce6f
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-09 18:08:02 +0900

    Add array_unique() examples

--- playground/php/test.php
+++ playground/php/test.php
@@ -3,5 +3,14 @@
 //echo("Hello, world!");
 
 # 2. Generate a random int
-$foo = rand(0, 1);
-echo $foo;
+//$foo = rand(0, 1);
+//echo $foo;
+
+# 3. Removes duplicate values
+$foo1 = array('A', 'B', 'C', 'D', 'E', 'B', 'C');
+$bar1 = array_unique($foo1);
+print_r($bar1);
+echo PHP_EOL;
+$foo2= array(2, 3, 4, 5, 4, 3, 2, 1);
+$bar2 = array_unique($foo2);
+print_r($bar2);
