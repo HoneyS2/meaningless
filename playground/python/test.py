49a4956ec578953b6454a3ad9ba27bc7ed963d46
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-13 19:09:21 +0900

    Add time module

--- playground/python/test.py
+++ playground/python/test.py
@@ -1,7 +1,16 @@
-import datetime
+#import datetime
+import time
 
-print("Hello, world!")
+#print("Hello, world!")
 
+'''
 now = datetime.datetime.now()
 print(now)
 print('{:%Y-%m-%d %H:%M:%S}'.format(now))
+'''
+
+start = time.time()
+for i in range(100):
+    print("Hello, world!")
+end = time.time()
+print(end - start)
