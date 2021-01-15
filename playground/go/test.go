8238de3d4a657791936f5e0d2396581268f7a922
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-15 19:05:20 +0900

    Add slice init

--- playground/go/test.go
+++ playground/go/test.go
@@ -9,10 +9,15 @@ func main() {
     //fmt.Println("Hello, world!")
     //fmt.Println(time.Now())
 
+    /*
     foo := map[int]string{
         0: "a",
         1: "b",
         2: "c",
     }
+    fmt.Println(foo)
+    */
+
+    foo := []string{"a", "b", "c"}
     fmt.Println(foo)
 }
