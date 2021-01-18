34de56040e96a07ceba8d685088acef4a9f236e9
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-18 21:34:27 +0900

    Add funny joke 2

--- playground/javascript/test.js
+++ playground/javascript/test.js
@@ -14,6 +14,14 @@ foo(1);
 /*
 funny jokes #1
 */
+/*
 console.log(0 == "0");
 console.log(0 == []);
 console.log("0" == []);
+*/
+
+/*
+funny jokes #2
+*/
+console.log(1 + "1");
+console.log(1 - "1");
