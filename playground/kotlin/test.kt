dc1400654aacc6dfb32b50037d3c54a260f3eb2c
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-24 20:25:54 +0900

    Add trimMargin example

--- playground/kotlin/test.kt
+++ playground/kotlin/test.kt
@@ -1,13 +1,22 @@
 fun main() {
     //println("Hello, world!")
     
+    /*
     var a: String
     var b: String
     a = foo()
     b = foo("bar2")
     println(a+" "+b)
+    */
+    
+    var str = """A
+                |B
+                |C""".trimMargin()
+    println(str)
 }
 
+/*
 fun foo(str: String = "bar"): String {
     return str
 }
+*/
