e283f7f56e9e68f4081b5e3c6587f8be7175f746
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-25 21:40:12 +0900

    Add method for type

--- playground/go/test.go
+++ playground/go/test.go
@@ -2,8 +2,17 @@ package main
 
 import (
     "fmt"
+    "math"
     //"time"
 )
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
 
 func main() {
     //fmt.Println("Hello, world!")
@@ -18,6 +27,11 @@ func main() {
     fmt.Println(foo)
     */
 
+    /*
     foo := []string{"a", "b", "c"}
     fmt.Println(foo)
+    */
+    
+    v := Vertex{3, 4}
+    fmt.Println(v.Abs())
 }
