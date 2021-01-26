4ff897a6cb6612ff2da0152f5e1b0733fb4bdc86
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-26 23:07:12 +0900

    Add Scale method with pointer receiver

--- playground/go/test.go
+++ playground/go/test.go
@@ -14,6 +14,11 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func main() {
     //fmt.Println("Hello, world!")
     //fmt.Println(time.Now())
@@ -33,5 +38,7 @@ func main() {
     */
     
     v := Vertex{3, 4}
+    fmt.Println(v.Abs())
+    v.Scale(10)
     fmt.Println(v.Abs())
 }
