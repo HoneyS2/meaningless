889a8bcec5ed5d04e9f9fb1fb44abba313e2204c
HoneyS2 <Symbiote.Sim@gmail.com>
2021-01-28 21:33:54 +0900

    Add make example & fix indentation

--- playground/go/test.go
+++ playground/go/test.go
@@ -1,44 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    //"time"
+        "fmt"
+        //"math"
+        //"time"
 )
 
+/*
 type Vertex struct {
-	X, Y float64
+        X, Y float64
 }
 
 func (v Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+        return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+        v.X = v.X * f
+        v.Y = v.Y * f
 }
+*/
 
 func main() {
-    //fmt.Println("Hello, world!")
-    //fmt.Println(time.Now())
+        //fmt.Println("Hello, world!")
+        //fmt.Println(time.Now())
 
-    /*
-    foo := map[int]string{
-        0: "a",
-        1: "b",
-        2: "c",
-    }
-    fmt.Println(foo)
-    */
+        /*
+        foo := map[int]string{
+                0: "a",
+                1: "b",
+                2: "c",
+        }
+        fmt.Println(foo)
+        */
 
-    /*
-    foo := []string{"a", "b", "c"}
-    fmt.Println(foo)
-    */
-    
-    v := Vertex{3, 4}
-    fmt.Println(v.Abs())
-    v.Scale(10)
-    fmt.Println(v.Abs())
+        /*
+        foo := []string{"a", "b", "c"}
+        fmt.Println(foo)
+        */
+
+        /*
+        v := Vertex{3, 4}
+        fmt.Println(v.Abs())
+        v.Scale(10)
+        fmt.Println(v.Abs())
+        */
+
+        foo := make([]string, 10, 15)
+        fmt.Println(len(foo), cap(foo))
 }
