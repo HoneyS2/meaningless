b8d931654da12fa7944a16fada8606b60988f270
HoneyS2 <Symbiote.Sim@gmail.com>
2021-02-04 20:30:52 +0900

    Add scanner input example

--- playground/java/Test.java
+++ playground/java/Test.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 class Test {
     public static void main(String args[]) {
-        System.out.println("Hello, world!");
+        //System.out.println("Hello, world!");
+
+        String foo = "";
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input text: ");
+        foo = scan.next();
+        System.out.println("Result: "+foo);
     }
 }
