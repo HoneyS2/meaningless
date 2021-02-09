4b4ed7001a17ac1b4293a97c6a41eb0c37ac46cc
HoneyS2 <Symbiote.Sim@gmail.com>
2021-02-09 17:11:41 +0900

    Add data structures example

--- playground/python/test.py
+++ playground/python/test.py
@@ -1,5 +1,5 @@
 #import datetime
-import time
+#import time
 
 #print("Hello, world!")
 
@@ -9,8 +9,21 @@ print(now)
 print('{:%Y-%m-%d %H:%M:%S}'.format(now))
 '''
 
+'''
 start = time.time()
 for i in range(100):
     print("Hello, world!")
 end = time.time()
 print(end - start)
+'''
+
+a_list = ['a', 'b', 'c', 'd']
+b_tuple = (1,2,3,4)
+c_dictionary = {'a': 'apple', 'b': 'banana', 'c': 3}
+d_set = set([1,3,2])
+e_set2 = set('Hello, world!')
+print(a_list)
+print(b_tuple)
+print(c_dictionary)
+print(d_set)
+print(e_set2)
