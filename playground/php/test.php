3bdae47470d7f847daf93860a90fc9271f8680ce
HoneyS2 <Symbiote.Sim@gmail.com>
2021-03-13 20:02:06 +0900

    Add string test

--- playground/php/test.php
+++ playground/php/test.php
@@ -7,6 +7,7 @@
 //echo $foo;
 
 # 3. Removes duplicate values
+/*
 $foo1 = array('A', 'B', 'C', 'D', 'E', 'B', 'C');
 $bar1 = array_unique($foo1);
 print_r($bar1);
@@ -14,3 +15,10 @@ echo PHP_EOL;
 $foo2= array(2, 3, 4, 5, 4, 3, 2, 1);
 $bar2 = array_unique($foo2);
 print_r($bar2);
+*/
+
+# 4. String test
+$str1 = "foo";
+$str2 = "bar";
+$str3 = "$str1, $str2";
+print_r($str3);
