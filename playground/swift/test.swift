73f136c75a363f7b3977ca9dc27dafffacf42ac4
HoneyS2 <Symbiote.Sim@gmail.com>
2021-04-27 23:47:16 +0900

    Add optional

--- playground/swift/test.swift
+++ playground/swift/test.swift
@@ -1,9 +1,14 @@
 //print("Hello, world!")
 
-var foo : String
-let bar : Int
+/*
+var foo: String
+let bar: Int
 
 foo = "ABC"
 bar = 123
 //print(foo, "and", bar)
 print("\(foo) and \(bar)")
+*/
+
+var foo: String?
+print(foo)
