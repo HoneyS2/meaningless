6215829487db5616c431571ad4742311eaa28727
HoneyS2 <Symbiote.Sim@gmail.com>
2021-04-28 23:27:35 +0900

    Add convert a String to an Int

--- playground/swift/test.swift
+++ playground/swift/test.swift
@@ -10,5 +10,15 @@ bar = 123
 print("\(foo) and \(bar)")
 */
 
+/*
 var foo: String?
 print(foo)
+*/
+
+var bar: String = "123"
+print(bar)
+print(type(of:bar))
+//var baz = Int(bar)
+var baz: Int = Int(bar) ?? 0
+print(baz)
+print(type(of:baz))
