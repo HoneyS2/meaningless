5e4940f0001d37a2eb617cc604d4c77788071a26
HoneyS2 <Symbiote.Sim@gmail.com>
2021-07-05 23:37:48 +0900

    Add function to print string

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -1,7 +1,18 @@
 fn main() {
     // println!("Hello, world!");
+
+    /*
     let mut foo = "bar";
     println!("{}", foo);
     foo = "baz";
     println!("{}", foo);
+    */
+
+    let foo = "world";
+    let bar = foo.to_string();
+    hello_str(bar);
+}
+
+fn hello_str(str: String) {
+    println!("Hello, {}!", str);
 }
