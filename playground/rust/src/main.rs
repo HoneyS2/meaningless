f88f003906877a5ad01b994d98536b49f4e7fcb8
HoneyS2 <Symbiote.Sim@gmail.com>
2021-07-08 23:34:19 +0900

    Add array

--- playground/rust/src/main.rs
+++ playground/rust/src/main.rs
@@ -17,16 +17,25 @@ fn main() {
     hello_str(foe);
     */
 
+    /*
     let baz: u128 = 1234;
     print_num(baz);
+    */
+
+    let a = [1, 2, 3, 4, 5];
+    let b = a[0];
+    let c = a[1];
+
+    println!("{:?}", a);
+    println!("{}, {}", b, c);
 }
 
 /*
 fn hello_str(str: String) {
     println!("Hello, {}!", str);
 }
-*/
 
 fn print_num(num: u128) {
     println!("{}", num);
 }
+*/
