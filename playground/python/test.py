8ea8edfb602a7f55a59a8a44d2bde12f4e634b44
HoneyS2 <Symbiote.Sim@gmail.com>
2021-08-26 23:05:14 +0900

    Add sys module

--- playground/python/test.py
+++ playground/python/test.py
@@ -9,7 +9,8 @@
 #from decimal import *
 #import random
 #import textwrap
-import pprint
+#import pprint
+import sys
 
 #print("Hello, world!")
 
@@ -368,8 +369,14 @@ bar = textwrap.fill(foo, width=65)
 print(bar)
 '''
 
+'''
 foo = ['a', 'b', 'c', 'd', 'e', 'Hello, world!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
 pp = pprint.PrettyPrinter(indent=4)
 pp2 = pprint.PrettyPrinter(width=41, compact=True)
 pp.pprint(foo)
 pp2.pprint(foo)
+'''
+
+print(sys.argv)
+sys.stderr.write('Goodbye, world!\n')
+sys.exit()
