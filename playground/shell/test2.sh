262f149f7db1992913379ed3dca32d0247b3ec9e
HoneyS2 <Symbiote.Sim@gmail.com>
2021-09-08 23:35:03 +0900

    Add associative array loop with order

--- playground/shell/test2.sh
+++ playground/shell/test2.sh
@@ -1,8 +1,17 @@
 #!/bin/zsh
 
+: <<END
 declare -A assoc_array
 assoc_array=( a A b B c C )
 
 for key val in "${(@kv)assoc_array}"; do
     echo "$key -> $val"
+done
+END
+
+declare -A array=( 1 A 2 B 3 C )
+declare -a order=( 1 2 3 )
+for i in "${order[@]}"
+do
+    echo "$i: ${array[$i]}"
 done
