fb1273616dda3bcb40dbf359f04525c3d1779146
HoneyS2 <Symbiote.Sim@gmail.com>
2021-09-27 20:34:54 +0900

    Add vars

--- playground/python/test.py
+++ playground/python/test.py
@@ -469,6 +469,7 @@ class car:
 
 foo = car('UltraSuper', 351)
 setattr(foo, 'engine', 'V12')
-print(foo.name, foo.speed)
-print(getattr(foo, 'name'), getattr(foo, 'speed'), getattr(foo, 'price', 600000))
+#print(foo.name, foo.speed)
+#print(getattr(foo, 'name'), getattr(foo, 'speed'), getattr(foo, 'price', 600000))
 print(foo.__dict__)
+print(vars(foo))
