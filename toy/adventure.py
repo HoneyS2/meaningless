7b8125e743b55c600b56122bc2dbc1178211aac9
HoneyS2 <Symbiote.Sim@gmail.com>
2021-10-23 18:21:57 +0900

    Keep going 2

--- toy/adventure.py
+++ toy/adventure.py
@@ -1,2 +1,13 @@
+from time import sleep
+
 print('Once upon a time,')
+sleep(1)
 print('There was a guy.')
+sleep(1)
+for i in range(0, 3):
+    print('.', end='', flush=True)
+    sleep(1.1)
+print('')
+name = input('=> What is the guy\'s name? : ')
+sleep(1)
+print('His name was "'+name+'".')
