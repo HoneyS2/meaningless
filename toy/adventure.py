eafaad1dcca49d87b267afd1515be3db554913ff
HoneyS2 <Symbiote.Sim@gmail.com>
2021-10-24 20:34:07 +0900

    Keep going 3

--- toy/adventure.py
+++ toy/adventure.py
@@ -11,3 +11,21 @@ print('')
 name = input('=> What is the guy\'s name? : ')
 sleep(1)
 print('His name was "'+name+'".')
+sleep(1)
+print('1. Teleport')
+print('2. Time travel')
+while True:
+    try:
+        power = int(input('=> What is the guy\'s super power? : '))
+        if power == 1:
+            power_str = 'Teleport'
+            break;
+        elif power == 2:
+            power_str = 'Time travel'
+            break;
+        else:
+            print(str(power)+' is not an option yet.')
+    except ValueError:
+        print('Wrong answer')
+sleep(1)
+print(name+' has a super power. That\'s '+power_str+'.')
