b06e9a8eec10a1e4adb520e262b9cf5509308eb2
HoneyS2 <Symbiote.Sim@gmail.com>
2021-11-04 21:45:39 +0900

    Add outer join

--- playground/sql/test.sql
+++ playground/sql/test.sql
@@ -7,6 +7,7 @@ SELECT `COLUMN` FROM `table` WHERE id = 1;
 SELECT `COLUMN` FROM `table_A` AS A INNER JOIN `table_B` AS B ON A.Key = B.Key;
 SELECT `COLUMN` FROM `table_A` AS A LEFT JOIN `table_B` AS B ON A.Key = B.Key;
 SELECT `COLUMN` FROM `table_A` AS A RIGHT JOIN `table_B` AS B ON A.Key = B.Key;
+SELECT `COLUMN` FROM `table_A` AS A FULL OUTER JOIN `table_B` AS B ON A.Key = B.Key;
 
 # DISTINCT
 SELECT DISTINCT `COLUMN` FROM `table`;
