48ec9c4d7e58675a8e0256509fa5e4b1df7c5f9c
HoneyS2 <Symbiote.Sim@gmail.com>
2021-12-23 22:22:34 +0900

    Add array_key_exists

--- playground/php/test.php
+++ playground/php/test.php
@@ -95,11 +95,13 @@ print_r($foo);
 
 # 14. filter_var
 # ref. https://www.php.net/manual/en/filter.filters.validate.php
+/*
 $foo = "https://www.php.net";
 print_r(filter_var($foo, FILTER_VALIDATE_URL));
 print_r(PHP_EOL);
 $bar = "aaa@bbb.ccc";
 print_r(filter_var($bar, FILTER_VALIDATE_EMAIL));
+*/
 
 # 15. http_build_query
 /*
@@ -321,3 +323,7 @@ $foo = ['a', 'b', 'c', 'd', 'e'];
 shuffle($foo);
 print_r($foo);
 */
+
+# 40. array_key_exists
+$foo = ['hello' => 'world'];
+print_r(array_key_exists('hello', $foo));
