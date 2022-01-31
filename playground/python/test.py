98ed3516ef35264d58f3120f80b51899060ca5e9
HoneyS2 <Symbiote.Sim@gmail.com>
2022-01-31 09:22:54 +0900

    Add an example difference between two lists with for loop

--- playground/python/test.py
+++ playground/python/test.py
@@ -614,5 +614,13 @@ baz = "Hello, World!"
 print(baz.istitle())
 '''
 
+'''
 foo = ['a', 'b', 'c']
 print(",".join(foo))
+'''
+
+foo = [1, 2, 3, 4, 5]
+bar = [1, 2, 3, 6, 7]
+for index, (first, second) in enumerate(zip(foo, bar)):
+    if first != second:
+        print(index, first, second)
