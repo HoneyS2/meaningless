19e1922927ba0b80085dc99a16d4272be7aaee80
HoneyS2 <Symbiote.Sim@gmail.com>
2022-02-01 20:03:57 +0900

    Comment out

--- playground/php/test.php
+++ playground/php/test.php
@@ -417,9 +417,11 @@ print_r(array_pad($foo, -5, 0));
 */
 
 # 52. array_intersect_key
+/*
 $foo = ['a' => 1, 'b' => 2, 'c' => 3];
 $bar = ['a' => 4, 'e' => 5, 'f' => 6];
 print_r(array_intersect_key($foo, $bar));
+*/
 
 # 53. current, prev, next, reset
 $foo = [1, 2, 3, 4, 5];
