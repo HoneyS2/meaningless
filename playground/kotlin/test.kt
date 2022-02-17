2e6547d79bfa3c8c0c5ee489602e4d40c3504b7d
HoneyS2 <Symbiote.Sim@gmail.com>
2022-02-17 23:49:57 +0900

    Add ListIterator

--- playground/kotlin/test.kt
+++ playground/kotlin/test.kt
@@ -1,4 +1,4 @@
-import java.util.Scanner
+//import java.util.Scanner
 
 fun main() {
     //println("Hello, world!")
@@ -18,12 +18,30 @@ fun main() {
     println(str)
     */
 
+    /*
     print("Input text: ")
     val scan: Scanner = Scanner(System.`in`)
     var str = scan.nextLine()
     print("Input integer: ")
     var num = scan.nextInt()
     println("Result: $str $num")
+    */
+
+    val numbers = listOf("a", "b", "c", "d", "e")
+    val listIterator = numbers.listIterator()
+
+    println("Iterating forwards:")
+    while (listIterator.hasNext()) {
+        print("Index: ${listIterator.nextIndex()}")
+        println(", value: ${numbers[listIterator.nextIndex()]}")
+        listIterator.next()
+    }
+
+    println("Iterating backwards:")
+    while (listIterator.hasPrevious()) {
+        print("Index: ${listIterator.previousIndex()}")
+        println(", value: ${listIterator.previous()}")
+    }
 }
 
 /*
