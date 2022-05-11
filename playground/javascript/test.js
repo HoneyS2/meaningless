280cee1b38f1ed9ea978aa9bbd9f7ece18258b9e
HoneyS2 <Symbiote.Sim@gmail.com>
2022-05-11 23:34:06 +0900

    Add date methods 5 - getTime

--- playground/javascript/test.js
+++ playground/javascript/test.js
@@ -295,6 +295,7 @@ console.log(foo.getHours());
 console.log(foo.getMinutes());
 console.log(foo.getSeconds());
 console.log(foo.getMilliseconds());
+console.log(foo.getTime());
 
 /*
 let foo = ['a', 'b', 'c', 'a', 'b', 'c'];
